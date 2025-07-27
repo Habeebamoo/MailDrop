527933e2d6e4138e3e1b67c3184e56958f21a8f7
Habeebamoo <habeebamoo08@gmail.com>
2025-07-27 21:20:57 +0100

    updated responsiveness

--- client/src/components/home/Hero.tsx
+++ client/src/components/home/Hero.tsx
@@ -5,26 +5,26 @@ import heroImg from "../../assets/hero.png"
 const Hero = () => {
   return (
     <section className="pt-[100px] pb-5 bg-bg px-2">
-      <div className="sm:flex-between sm:w-[80%] mx-auto">
+      <div className="lg:flex-between lg:w-[70%] mx-auto">
         <div>
-          <div className="max-sm:flex-center sm:flex-start">
+          <div className="max-lg:flex-center lg:flex-start">
             <p className="bg-bg2 font-outfit flex-start py-1 px-2 rounded font-open text-sm text-primary">
             <FaStar color="gold" />
             <span className="ml-1">New: Email Marketing Platform</span>
             </p>
           </div>
-          <h1 className="font-inter text-primary text-3xl max-sm:text-center mt-3">Email Made Simple & Secure</h1>
-          <p className="max-sm:text-center text-sm max-sm:mt-3 sm:mt-1 text-accent">
+          <h1 className="font-inter text-primary text-3xl max-lg:text-center mt-3">Email Made Simple & Secure</h1>
+          <p className="max-lg:text-center text-sm max-lg:mt-3 lg:mt-1 text-accent max-lg:w-[90%] mx-auto">
             Experience the future of email with MailDrop. Intelligent organization, bulletproof security and lightning-fast perfomance in one beautiful package
           </p>
-          <button className="max-sm:mx-auto max-sm:mt-4 sm:mt-2 px-4 btn-primary flex-center">
+          <button className="max-lg:mx-auto max-lg:mt-4 lg:mt-2 px-4 btn-primary flex-center">
             <span className="mr-2">Start Free Trail</span>
             <FaArrowRight size={14} />
           </button>
         </div>
         <div className="max-sm:mt-4">
           <div className="flex-center">
-            <img src={heroImg} className="max-sm:h-[350px] sm:h-[300px]" />
+            <img src={heroImg} className="max-lg:h-[350px] lg:h-[300px]" />
           </div>
         </div>
       </div>
--- client/src/components/home/Main.tsx
+++ client/src/components/home/Main.tsx
@@ -1,7 +1,44 @@
+import { FiEdit, FiLink, FiMail, FiUsers } from "react-icons/fi"
+
 const Main = () => {
   return (
-    <section>
-
+    <section className="py-6 px-3">
+      <div className="flex-center">
+        <p className="bg-bg2 font-outfit py-1 px-2 rounded font-open text-sm text-primary">
+        <span className="ml-1">Features</span>
+        </p>
+      </div>
+      <p className="text-sm text-center mt-1 mx-auto text-accent w-[80%]">Everything you need to market through emails is provided by the platform</p>
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 lg:gap-3 md:gap-3 mt-5 p-2 lg:w-[95%] mx-auto">
+        <div className="bg-bg p-4 rounded-md mb-4">
+          <div className="bg-primary p-2 rounded-md inline-block">
+            <FiEdit color="white" size={25} />
+          </div>
+          <h1 className="font-open mt-1 text-xl">Campaign Builder</h1>
+          <p className="text-accent text-sm mt-1">Create personalized campaigns with ease, add descriptions, upload lead magnets and get a sharable link instantly</p>
+        </div>
+        <div className="bg-bg p-4 rounded-md mb-4">
+          <div className="bg-primary p-2 rounded-md inline-block">
+            <FiUsers color="white" size={25} />
+          </div>
+          <h1 className="font-open mt-1 text-xl">Leads Organization</h1>
+          <p className="text-accent text-sm mt-1">Collect, view and manage all your leads, sort by any category and export data for later use</p>
+        </div>
+        <div className="bg-bg p-4 rounded-md mb-4">
+          <div className="bg-primary p-2 rounded-md inline-block">
+            <FiLink color="white" size={25} />
+          </div>
+          <h1 className="font-open mt-1 text-xl">Unique Sharable Links</h1>
+          <p className="text-accent text-sm mt-1">Generate campaigns-specific links to share across social media, email or website in both text and QR CODE format</p>
+        </div>
+        <div className="bg-bg p-4 rounded-md mb-4">
+          <div className="bg-primary p-2 rounded-md inline-block">
+            <FiMail color="white" size={25} />
+          </div>
+          <h1 className="font-open mt-1 text-xl">Email Marketing</h1>
+          <p className="text-accent text-sm mt-1">Send targeted promotional or follow-up emails to your leads directly from your dashboard</p>
+        </div>
+      </div>
     </section>
   )
 }
