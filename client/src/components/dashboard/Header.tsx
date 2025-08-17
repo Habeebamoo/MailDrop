376abc044a6a2a422f77f65ca56323b8af963d2c
Habeebamoo <habeebamoo08@gmail.com>
2025-08-17 19:03:51 +0100

    updated dashboard header

--- client/src/components/dashboard/Header.tsx
+++ client/src/components/dashboard/Header.tsx
@@ -1,6 +1,13 @@
 import logo from "../../assets/logo.png"
+import { useUser } from "../../context/UserContext"
 
 const Header = () => {
+  const { user } = useUser()
+
+  const getFirstLetterOf = (text: string | undefined) => {
+    return text?.charAt(0).toUpperCase()
+  }
+
   return (
     <header className="p-4 bg-white dark:bg-gray-900 dark:border-gray-800 dark:shadow-lg z-40 fixed top-0 left-0 right-0 border-b-1 border-b-accentLight flex-between">
       <div className="flex-start dark:text-primary dark:text-white">
@@ -8,7 +15,7 @@ const Header = () => {
         <h1 className="text-xl font-outfit ml-1">MailDrop</h1>
       </div>
       <div>
-        <div className="h-7 w-7 rounded-full bg-accentLight border-1 border-accent flex-center text-white dark:text-black">H</div>
+        <div className="h-7 w-7 rounded-full bg-accentLight border-1 border-accent flex-center text-white dark:text-black">{getFirstLetterOf(user?.name)}</div>
       </div>
     </header>
   )
