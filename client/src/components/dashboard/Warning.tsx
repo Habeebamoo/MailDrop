43b9815649ad1bde24119a85fb719536ed89d597
Habeebamoo <habeebamoo08@gmail.com>
2025-08-22 13:54:06 +0100

    updated Warning model

--- client/src/components/dashboard/Warning.tsx
+++ client/src/components/dashboard/Warning.tsx
@@ -17,7 +17,7 @@ const Warning = ({ title, text, setWarning, confirmAction }: PropsType) => {
       <div className="p-6 bg-white rounded-sm border-1 border-accentLight flex-center flex-col">
         <PiWarningFill size={40} className="mb-2" color="#231e88" />
         <h1 className="font-inter text-xl mb-2">{title}</h1>
-        <p className="text-sm text-accent mb-2">{text}</p>
+        <p className="text-sm text-accent mb-2 text-center">{text}</p>
         <div className="flex-center gap-3 mt-1">
           <button onClick={cancelWarning} className="bg-red-400 border-red-400 hover:text-red-400 btn-primary">Cancel</button>
           <button onClick={confirmAction} className="btn-primary">Proceed</button>
