54c45c51a21372d4962be2288d78661a5690db58
Habeebamoo <habeebamoo08@gmail.com>
2025-08-26 00:08:22 +0100

    used UTC format for token ExpiresAt

--- server/internal/repositories/user.repo.go
+++ server/internal/repositories/user.repo.go
@@ -170,7 +170,7 @@ func (userRepo *UserRepo) CreateToken(userId uuid.UUID) (string, int, error) {
 	//create a token for verification
 	token := models.Token{
 		UserId: userId,
-		ExpiresAt: time.Now().Add(12 * time.Hour),
+		ExpiresAt: time.Now().Add(12 * time.Hour).UTC(),
 	}
 
 	err = userRepo.db.Create(&token).Error
