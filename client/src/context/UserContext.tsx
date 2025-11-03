9e84469261439f54e614cd0a4436b476b00c3219
Habeebamoo <habeebamoo08@gmail.com>
2025-11-03 10:18:00 +0100

    updated user context

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -23,23 +23,10 @@ type initUserContext = {
 const UserContext = createContext<initUserContext | undefined>(undefined)
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>({
-  userId: "d",
-  name: "habeeb",
-  email: "dd",
-  verified: true,
-  profile: {
-    profilePic: "",
-    bio: "",
-    totalCampaigns: 3,
-    totalSubscribers: 5,
-    totalClicks: 1,
-    totalEmails: 9,
-  }
-})
-  const [loading, setLoading] = useState<boolean>(false)
+  const [user, setUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
 
-  /*useEffect(() => {
+  useEffect(() => {
     let mounted = true;
 
     const fetchUser = async () => {
@@ -74,7 +61,7 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
     return () => {
       mounted = false;
     }
-  }, [])*/
+  }, [])
   
   return (
     <UserContext value={{ user, loading }}>
